d2774ab4e8976c5a9dd2ce7a4127127890b0ecfc
Eric Herot <eric.github@herot.com>
2014-03-07 15:24:21 -0500

    hadoop version bump 1.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_hadoop'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
 
-depends 'hadoop', '= 1.1.0'
+depends 'hadoop', '= 1.1.1'
