282c6644d8ca0d024b983b19f48630ea06cd0465
Eric Herot <eric.github@herot.com>
2014-07-22 14:35:14 -0400

    Bump storage cookbook version v2.1.3

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
 
 depends 'hadoop', '= 2.0.0'
-depends 'storage', '= 2.0.0'
+depends 'storage', '= 2.1.3'
