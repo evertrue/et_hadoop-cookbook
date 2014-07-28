b6876d706319e5dc153b2f82a655164c2d55bb9b
Eric Herot <eric.github@herot.com>
2014-07-28 13:44:19 -0400

    Bump hadoop v2.0.2

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures et_hadoop'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.1.0'
 
-depends 'hadoop', '= 2.0.1'
+depends 'hadoop', '= 2.0.2'
 depends 'storage', '= 2.1.3'
 depends 'pam', '= 1.0.1'
