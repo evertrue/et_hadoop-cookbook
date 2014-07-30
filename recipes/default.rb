4e58c684560fc671a5b821d74b05f23ce8395374
Eric Herot <eric.github@herot.com>
2014-07-30 15:12:02 -0400

    Switch to URL-style path for data_root

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,7 +12,7 @@ include_recipe 'storage'
 
 if node['storage'] && node['storage']['ephemeral_mounts']
   node.override['hadoop']['data_root'] =
-    node['storage']['ephemeral_mounts'].map { |em| "#{em}/data" }
+    node['storage']['ephemeral_mounts'].map { |em| "file:///#{em}/data" }
   node.override['hadoop']['tmp_root'] =
     node['storage']['ephemeral_mounts'].map { |em| "#{em}/tmp" }
 end
