7ee24517e48bb36d086e06dc274cf924c1700e42
Eric Herot <eric.github@herot.com>
2014-08-08 16:59:19 -0400

    Bump version v2.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_hadoop'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.1.6'
+version          '2.2.0'
 
 depends 'hadoop', '= 2.1.0'
 depends 'storage', '>= 2.1.7'
