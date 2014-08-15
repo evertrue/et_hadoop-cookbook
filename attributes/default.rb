fcfd8d07abbda1fb0aa95df7773af82c7465abda
Eric Herot <eric.github@herot.com>
2014-08-15 11:26:50 -0400

    Switch to community cookbook attributes (and drop the old ones)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,3 @@
 set['hadoop']['distribution'] = 'cdh'
-set['hadoop']['envfile']['HADOOP_OPTS'] = '-Djdk.xml.entityExpansionLimit=0'
-
-# Old cookbook:
-set['hadoop']['local_fqdn'] = node.name + '.priv.evertrue.com'
-set['hadoop']['env_default']['hadoop']['HADOOP_OPTS'] =
+set['hadoop']['envfile']['HADOOP_OPTS'] = '-Djdk.xml.entityExpansionLimit=0 ' \
   '-Djdk.xml.entityExpansionLimit=0'
