46c187939f3324bac36488677a003d2918ce507d
Eric Herot <eric.github@herot.com>
2014-08-15 11:17:05 -0400

    For namenode testing, move attributes that should be part of the wrapper cookbook all the time to attributes files

--- attributes/namenode.rb
+++ attributes/namenode.rb
@@ -1,3 +1,11 @@
 # set['java']['java_home'] = "/usr/lib/jvm/java-7-openjdk-amd64"
 # set['java']['java_exec'] = node['java']['java_home'] + "/bin/java"
 set['java']['jdk_version'] = '7'
+
+set['hadoop']['yarn_site'] = {
+  'yarn.resourcemanager.scheduler.address' => '0.0.0.0',
+  'yarn.resourcemanager.resource-tracker.address' => '0.0.0.0',
+  'yarn.resourcemanager.admin.address' => '0.0.0.0',
+  'yarn.resourcemanager.webapp.address' => '0.0.0.0',
+  'yarn.resourcemanager.address' => '0.0.0.0'
+}
