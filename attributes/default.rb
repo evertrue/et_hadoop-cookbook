15f195e530d3a925110770da521832a1b3734056
Eric Herot <eric.github@herot.com>
2014-08-15 14:36:58 -0400

    Hardcode default cluster name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 set['hadoop']['distribution'] = 'cdh'
 set['hadoop']['envfile']['HADOOP_OPTS'] = '-Djdk.xml.entityExpansionLimit=0 ' \
   '-Djdk.xml.entityExpansionLimit=0'
+set['hadoop']['cluster_name'] = 'main'
