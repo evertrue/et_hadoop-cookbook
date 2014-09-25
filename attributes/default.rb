a06d499752740d92e9aac11c42eadd2ab5fed730
Mark Greene <mark@evertrue.com>
2014-09-25 18:42:04 -0400

    Increase task heap size from 200MB to 1024MB

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,8 @@
 set['hadoop']['local_fqdn'] = node.name + '.priv.evertrue.com'
 set['hadoop']['env_default']['hadoop']['HADOOP_OPTS'] =
   '-Djdk.xml.entityExpansionLimit=0'
+set['hadoop']['env_default']['hadoop']['HADOOP_OPTS'] =
+  '-Djdk.xml.entityExpansionLimit=0'
 set['pam_d']['services']['su']['main'] = {
   'pam_limits' => {
     'interface' => 'session',
@@ -13,3 +15,5 @@ set['hadoop']['mapred-site']['mapred.tasktracker.map.tasks.maximum'] =
   node['cpu']['total']
 set['hadoop']['mapred-site']['mapred.tasktracker.reduce.tasks.maximum'] =
   node['cpu']['total']
+set['hadoop']['mapred-site']['mapred.child.java.opts'] =
+  -Xmx1024M
