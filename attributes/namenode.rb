6175741c83134ef0a40ccee96ae7630b23a33155
Jeff Byrnes <jeff@evertrue.com>
2014-10-28 22:34:01 -0400

    Allow `deploy` to act as mapred user for deploys
    Courtesy of the deployment config written for contacts_hadoop,
    this allows Hadoop jobs to be deployed without granting too much
    in the way of sudoers.

--- attributes/namenode.rb
+++ attributes/namenode.rb
@@ -1,3 +1,5 @@
 # set['java']['java_home'] = "/usr/lib/jvm/java-7-openjdk-amd64"
 # set['java']['java_exec'] = node['java']['java_home'] + "/bin/java"
 set['java']['jdk_version'] = '7'
+
+default['hadoop']['deploy_user'] = 'deploy'
--- recipes/namenode.rb
+++ recipes/namenode.rb
@@ -1,2 +1,13 @@
 include_recipe 'et_hadoop::default'
 include_recipe 'hadoop::namenode'
+
+# The deploy user must be allowed to copy files as mapred
+# However, sudoers is damned hard, so we allow all actions as mapred
+node.set['authorization']['sudo']['include_sudoers_d'] = true
+
+sudo 'hadoop' do
+  user node['hadoop']['deploy_user']
+  runas node['hadoop']['mapred_user']
+  nopasswd true
+  commands ['ALL']
+end
